6c6fc3c6f951576c5711a9da73fdafc3490e6526
delaquash <olaide1191@gmail.com>
2022-05-20 04:36:49 +0100

    implemented updateById for product router

--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,8 @@ const Product = require('../models/Products');
 const { verifyToken, verifyTokenAuthorization, verifyTokenAdmin } = require('./verifyToken');
 const CryptoJS = require("crypto-js");
 
-// create
+
+/* This is a post request to the server. It is creating a new product. */
 router.post('/',verifyTokenAdmin, async (req, res) => {
         const newProduct = new Product(req.body);
         try {
@@ -12,6 +13,28 @@ router.post('/',verifyTokenAdmin, async (req, res) => {
         } catch (error) {
             res.status(500).json(error)
         }
+})
+
+/* Updating the product. */
+router.put('/:id', verifyTokenAdmin, async (req, res) => {
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
+            $set: req.body,
+        }, {
+            new: true,
+        })
+        res.status(200).json(updatedProduct)
+    } catch (error) {
+      res.status(500).json(error)  
+    }
+})
+
+router.delete('/:id', verifyTokenAdmin, async (req, res)=> {
+    try {
+        const deletedProduct = await Product.findByIdAndDelete(req.params.id)
+    } catch (error) {
+        
+    }
 })
 
 module.exports = router
