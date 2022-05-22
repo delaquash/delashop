8700851aa43bc34f1a41a5a80017bbb3d89105fd
delaquash <olaide1191@gmail.com>
2022-05-22 19:44:41 +0100

    fix post income route

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const dotenv = require("dotenv")
 const authRouter = require('./routes/auth');
 const productRouter = require('./routes/product');
 const cartRouter = require('./routes/cart')
+const orderRouter = require('./routes/order')
+
 
 dotenv.config();
 
@@ -22,7 +24,8 @@ app.use(express.json())
 app.use('/api/user', user);
 app.use('/api/auth', authRouter);
 app.use('/api/products', productRouter);
-app.use('/api/carts', cartRouter)
+app.use('/api/carts', cartRouter);
+app.use('/api/order', orderRouter);
 
 app.listen(process.env.PORT  || 5000, () => {
     console.log('Backend Server is actively running!');
--- routes/order.js
+++ routes/order.js
@@ -3,6 +3,8 @@ const Order = require('../models/Order');
 const { verifyToken, verifyTokenAuthorization, verifyTokenAdmin } = require('./verifyToken');
 
 
+/* This is a post request to the order route. It is using the verifyToken middleware to verify that
+the user is logged in. If the user is logged in, it will create a new order in the database. */
 router.post('/',verifyToken, async (req, res) => {
     const newOrder = new Order(req.body);
     try {
@@ -13,6 +15,9 @@ router.post('/',verifyToken, async (req, res) => {
     }
 })
 
+/* This is a put request to the order route. It is using the verifyTokenAdmin middleware to verify that
+
+the user is an admin. If the user is an admin, it will update the order in the database. */
 router.put('/:id', verifyTokenAdmin, async(req, res) => {
     try {
         const updatedOrder = await Order.findByIdAndUpdate(
@@ -30,6 +35,9 @@ router.put('/:id', verifyTokenAdmin, async(req, res) => {
     }
 })
 
+/* This is a delete request to the order route. It is using the verifyTokenAdmin middleware to verify
+that
+the user is an admin. If the user is an admin, it will delete the order in the database. */
 router.delete('/:id', verifyTokenAdmin, async(req, res)=> {
     try {
         await Order.findByIdAndDelete(req.params.id)
@@ -39,6 +47,9 @@ router.delete('/:id', verifyTokenAdmin, async(req, res)=> {
     }
 });
 
+/* This is a get request to the order route. It is using the verifyTokenAuthorization middleware to
+verify that
+the user is logged in. If the user is logged in, it will return all the orders in the database. */
 router.get('/find/:userId', verifyTokenAuthorization, async(req, res)=> {
     try {
         const orders = await Order.find({userId: req.params.userId});
@@ -48,13 +59,46 @@ router.get('/find/:userId', verifyTokenAuthorization, async(req, res)=> {
     }
 })
 
-// router.get('/', verifyTokenAdmin, async(req, res)=> {
-//     try {
-//         const carts = await Cart.find();
-//         res.status(200).json(carts)
-//     } catch (error) {
-//         res.status(500).json(error) 
-//     }
-// })
+/* This is a get request to the order route. It is using the verifyTokenAdmin middleware to verify that
+
+the user is an admin. If the user is an admin, it will return all the orders in the database. */
+router.get('/', verifyTokenAdmin, async(req, res)=> {
+    try {
+        const orders = await Order.find();
+        res.status(200).json(orders)
+    } catch (error) {
+        res.status(500).json(error) 
+    }
+})
+
+/* This is a get request to the order route. It is using the verifyTokenAdmin middleware to verify that
+the order is an admin. If the order is an admin, it will return all the orders in the database. */
+router.get('/income', verifyTokenAdmin, async(req, res)=> {
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth()-1));
+    try {
+        const income = await Order.aggregate([
+            {
+                $match : {createdAt: { $gte: previousMonth}}
+            },
+            {
+                $project :{
+                    month: {$month: "$createdAt"},
+                    sales: "$amount"
+                }},
+                {
+                    $group: {
+                        _id:"$month",
+                        total: {$sum: "$sales"},
+                    },
+                },
+        ]);
+        res.status(200).json(income)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 
 module.exports = router;
