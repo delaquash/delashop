5a09a95dc6d2337baae684948b3186070f7e49ae
delaquash <olaide1191@gmail.com>
2022-05-31 09:58:59 +0100

    Fix wallet schema for payment integration

---
+++ models/Wallet.js
@@ -0,0 +1,15 @@
+const mongoose= require("mongoose");
+
+const WalletSchema = Schema(
+  {
+    balance: { type: Number, default: 0 },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = model("Wallet", WalletSchema);
