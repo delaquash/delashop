88493c70749096298d732e9a0cbe316eb010a478
delaquash <olaide1191@gmail.com>
2022-06-14 19:34:46 +0100

    Fixed third party packages in frontend

--- front-end/src/components/Slider.js
+++ front-end/src/components/Slider.js
@@ -1,6 +1,6 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons'
 import React from 'react'
-import { Container, Arrow, Wrapper, ImgContainer, Image, InfoContainer, Slide } from '../styles/Slider'
+import { Container, Arrow, Wrapper, ImgContainer, Image, InfoContainer, Slide, Title, Desc, Button } from '../styles/Slider'
 
 
 const Slider = () => {
@@ -10,12 +10,34 @@ const Slider = () => {
             <ArrowLeftOutlined  />    
         </Arrow>
         <Wrapper>
-           <Slide>
+           <Slide bg="green">
            <ImgContainer>
-                <Image src="https://images.unsplash.com/photo-1654878543861-36e3d79c00b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1828&q=80" />
+                <Image src="https://media.istockphoto.com/photos/mens-white-blank-tshirt-templatefrom-two-sides-natural-shape-on-for-picture-id1151955708?b=1&k=20&m=1151955708&s=170667a&w=0&h=g35sZS5Nw1P0HD8gqGrcl1t-oY-1bgmpxQcWn0RqBW4=" />
                 </ImgContainer>
             <InfoContainer>
-
+              <Title>SUMMER SALES AND UNLIMITED OFFERS</Title>
+              <Desc>Dont compromise on your look, get quality and affordable wears at discounted prices.</Desc>
+              <Button>BUY NOW</Button>
+            </InfoContainer>
+           </Slide>
+           <Slide bg="red">
+           <ImgContainer>
+                <Image src="https://media.istockphoto.com/photos/mens-white-blank-tshirt-templatefrom-two-sides-natural-shape-on-for-picture-id1151955708?b=1&k=20&m=1151955708&s=170667a&w=0&h=g35sZS5Nw1P0HD8gqGrcl1t-oY-1bgmpxQcWn0RqBW4=" />
+                </ImgContainer>
+            <InfoContainer>
+              <Title>SUMMER SALES AND UNLIMITED OFFERS</Title>
+              <Desc>Dont compromise on your look, get quality and affordable wears at discounted prices.</Desc>
+              <Button>BUY NOW</Button>
+            </InfoContainer>
+           </Slide>
+           <Slide bg="blue">
+           <ImgContainer>
+                <Image src="https://media.istockphoto.com/photos/mens-white-blank-tshirt-templatefrom-two-sides-natural-shape-on-for-picture-id1151955708?b=1&k=20&m=1151955708&s=170667a&w=0&h=g35sZS5Nw1P0HD8gqGrcl1t-oY-1bgmpxQcWn0RqBW4=" />
+                </ImgContainer>
+            <InfoContainer>
+              <Title>SUMMER SALES AND UNLIMITED OFFERS</Title>
+              <Desc>Dont compromise on your look, get quality and affordable wears at discounted prices.</Desc>
+              <Button>BUY NOW</Button>
             </InfoContainer>
            </Slide>
         </Wrapper>
