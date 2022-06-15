56bc695518a27700fadc2e0e5b673474d7dee608
delaquash <olaide1191@gmail.com>
2022-06-15 10:30:05 +0100

    Still on slider

--- front-end/src/styles/Slider.js
+++ front-end/src/styles/Slider.js
@@ -4,7 +4,7 @@ export const Container = styled.div`
     width: 100%;
     height: 100vh;
     display:  flex;
-    background-color: coral;
+    /* background-color: coral; */
     position: relative;
 `;
 
