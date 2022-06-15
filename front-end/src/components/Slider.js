60b659755cbe2889797c08255d5841253c212b3b
delaquash <olaide1191@gmail.com>
2022-06-15 11:28:34 +0100

    Fixed ongoing changes

--- front-end/src/components/Slider.js
+++ front-end/src/components/Slider.js
@@ -10,12 +10,34 @@ const Slider = () => {
             <ArrowLeftOutlined  />    
         </Arrow>
         <Wrapper>
-           <Slide>
+           <Slide bg="green">
            <ImgContainer>
-                <Image src="https://media.istockphoto.com/photos/white-tshirt-clothes-picture-id685710150?k=20&m=685710150&s=612x612&w=0&h=JY6IbMc3Ij-Bkn3jLJW4GnA9k_Cc2fdAopSq6RB5o2E=" />
+                <Image src="https://media.istockphoto.com/photos/mens-white-blank-tshirt-templatefrom-two-sides-natural-shape-on-for-picture-id1151955708?b=1&k=20&m=1151955708&s=170667a&w=0&h=g35sZS5Nw1P0HD8gqGrcl1t-oY-1bgmpxQcWn0RqBW4=" />
                 </ImgContainer>
             <InfoContainer>
-
+              <Title>SUMMER SALES AND UNLIMITED OFFERS</Title>
+              <Desc>Dont compromise on your look, get quality and affordable wears at discounted prices.</Desc>
+              <Button>BUY NOW</Button>
+            </InfoContainer>
+           </Slide>
+           <Slide bg="red">
+           <ImgContainer>
+                <Image src="https://media.istockphoto.com/photos/mens-white-blank-tshirt-templatefrom-two-sides-natural-shape-on-for-picture-id1151955708?b=1&k=20&m=1151955708&s=170667a&w=0&h=g35sZS5Nw1P0HD8gqGrcl1t-oY-1bgmpxQcWn0RqBW4=" />
+                </ImgContainer>
+            <InfoContainer>
+              <Title>SUMMER SALES AND UNLIMITED OFFERS</Title>
+              <Desc>Dont compromise on your look, get quality and affordable wears at discounted prices.</Desc>
+              <Button>BUY NOW</Button>
+            </InfoContainer>
+           </Slide>
+           <Slide bg="blue">
+           <ImgContainer>
+                <Image src="https://media.istockphoto.com/photos/mens-white-blank-tshirt-templatefrom-two-sides-natural-shape-on-for-picture-id1151955708?b=1&k=20&m=1151955708&s=170667a&w=0&h=g35sZS5Nw1P0HD8gqGrcl1t-oY-1bgmpxQcWn0RqBW4=" />
+                </ImgContainer>
+            <InfoContainer>
+              <Title>SUMMER SALES AND UNLIMITED OFFERS</Title>
+              <Desc>Dont compromise on your look, get quality and affordable wears at discounted prices.</Desc>
+              <Button>BUY NOW</Button>
             </InfoContainer>
            </Slide>
         </Wrapper>
