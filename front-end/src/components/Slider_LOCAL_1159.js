2624316f44c6aeb7d7b5e31f302b3d8f3f5637f7
delaquash <olaide1191@gmail.com>
2022-06-20 14:50:34 +0100

    Fixed styling latest component

---
+++ front-end/src/components/Slider_LOCAL_1159.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons'
+import { sliderItems } from '../data'
+import { Container, Arrow, Wrapper, ImgContainer, Image, InfoContainer, Slide, Title, Desc, Button } from '../styles/Slider';
+
+const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0)
+  const handleClick=(direction)=> {
+    if(direction === "left"){
+      setSlideIndex(slideIndex > 0 ? slideIndex -1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex +1 : 0);
+    }
+  }
+  return (
+    <Container>
+        <Arrow direction="left" 
+          onClick={()=>handleClick("left")}
+        >
+            <ArrowLeftOutlined  />    
+        </Arrow>
+        <Wrapper slideIndex={slideIndex}>
+           {sliderItems.map((item) => (
+            <Slide bg={item.bg} key={item.id}>
+            <ImgContainer>
+                 <Image src={item.img} />
+                 </ImgContainer>
+             <InfoContainer>
+               <Title>{item.title}</Title>
+               <Desc>{item.desc}</Desc>
+               <Button>BUY NOW</Button>
+             </InfoContainer>
+            </Slide>
+           ))}
+        </Wrapper>
+        <Arrow direction="right"
+           onClick={()=>handleClick("left")}
+        >
+            <ArrowRightOutlined  />    
+        </Arrow>
+    </Container>
+  )
+}
+
+export default Slider
