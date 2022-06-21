f1638d1ec63bef71a473db4ae1b1603436748c6e
delaquash <olaide1191@gmail.com>
2022-06-21 14:55:00 +0100

    fixed logo for footer

---
+++ front-end/src/components/Footer.js
@@ -0,0 +1,20 @@
+import { Container, Logo, Desc, SocialContainer, SocialIcon,Center, Title, List, ListItem, Right, ContactItem, Left } from '../styles/Footer';
+
+
+const Footer = () => {
+  return (
+    <Container>
+        <Left>
+            <Logo>DELASHOP</Logo>
+            <Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Magnam architecto explicabo reiciendis minima deleniti ullam quod modi! 
+                Aspernatur quis, consequatur delectus expedita maxime, quos perferendis officia fugit minus, vero quae.
+           </Desc>
+        </Left>
+        <Center></Center>
+        <Right></Right>
+    </Container>
+  )
+}
+
+export default Footer
