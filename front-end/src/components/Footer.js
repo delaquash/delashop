07f382b3b33d936f5afaeeaee5c3ea147df80abc
delaquash <olaide1191@gmail.com>
2022-06-21 20:53:07 +0100

    Fixed social media pages in footer

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -1,3 +1,4 @@
+import { Facebook, Instagram, Twitter, YouTube } from '@material-ui/icons';
 import { Container, Logo, Desc, SocialContainer, SocialIcon,Center, Title, List, ListItem, Right, ContactItem, Left } from '../styles/Footer';
 
 
@@ -11,6 +12,20 @@ const Footer = () => {
                 Magnam architecto explicabo reiciendis minima deleniti ullam quod modi! 
                 Aspernatur quis, consequatur delectus expedita maxime, quos perferendis officia fugit minus, vero quae.
            </Desc>
+           <SocialContainer>
+            <SocialIcon>
+              <Facebook />
+            </SocialIcon>
+            <SocialIcon>
+              <Twitter />
+            </SocialIcon>
+            <SocialIcon>
+              <Instagram />
+            </SocialIcon>
+            <SocialIcon>
+              <YouTube />
+            </SocialIcon>
+           </SocialContainer>
         </Left>
         <Center></Center>
         <Right></Right>
