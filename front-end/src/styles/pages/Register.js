c2d1fb62229902ca9e3c1bfb866a25807db5e253
delaquash <olaide1191@gmail.com>
2022-06-29 17:06:32 +0100

    Implemented Register page

--- front-end/src/styles/pages/Register.js
+++ front-end/src/styles/pages/Register.js
@@ -1,6 +1,24 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+        (255, 255, 255, 0.2),
+        (255, 255, 255, 0.2)
+    );
+    background-image:url('https://images.pexels.com/photos/128299/pexels-photo-128299.jpeg?cs=srgb&dl=pexels-mabel-amber-128299.jpg&fm=jpg');
+    background-position: center; /* Center the image */
+    background-repeat: no-repeat; /* Do not repeat the image */
+    background-size: cover; /* Resize the background image to cover the entire container */
+    display: flex;
+    align-items: center;
+    justify-content: center; 
+    background-image: linear-gradient(
+        (255, 255, 255, 0.2),
+        (255, 255, 255, 0.2)
+    );
+`;
 export const Title = styled.h1``;
 export const Wrapper  = styled.div``;
 export const Form = styled.form``;
