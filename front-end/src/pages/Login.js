e17644532ac1231fb568affe68144df309974092
delaquash <olaide1191@gmail.com>
2022-06-30 15:03:59 +0100

    Fixed login page

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,23 +1,23 @@
-// import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom'
 
 
-// import { Container, Wrapper, Title, Form, Input, Button } from '../styles/pages/Login';
+import { Container, Wrapper, Title, Form, Input, Button, Link } from '../styles/pages/Login';
 
-// const Login = () => {
-//   return (
-//     <Container>
-//         <Wrapper>
-//             <Title>Login</Title>
-//             <Form>
-//                 <Input placeholder="Username" />
-//                 <Input placeholder="Password" />
-//                 <Button>SIGN IN</Button>
-//                 <Link>Forgot Password</Link>
-//                 <Link>Sign Up</Link>
-//             </Form>
-//         </Wrapper>
-//     </Container>
-//   )
-// }
+const Login = () => {
+  return (
+    <Container>
+        <Wrapper>
+            <Title>Login</Title>
+            <Form>
+                <Input placeholder="Username" />
+                <Input placeholder="Password" />
+                <Button>SIGN IN</Button>
+                <Link>Forgot Password</Link>
+                <Link>Sign Up</Link>
+            </Form>
+        </Wrapper>
+    </Container>
+  )
+}
 
-// export default Login
+export default Login
--- front-end/src/styles/pages/Login.js
+++ front-end/src/styles/pages/Login.js
@@ -3,12 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
     width: 100vw;
     height: 100vh;
-    background: 
-    /* linear-gradient(
-        (255, 255, 255, 0.2),
-        (255, 255, 255, 0.2)
-    ), */
-    url('https://images.pexels.com/photos/128299/pexels-photo-128299.jpeg?cs=srgb&dl=pexels-mabel-amber-128299.jpg&fm=jpg');
+    background: url('https://images.pexels.com/photos/128299/pexels-photo-128299.jpeg?cs=srgb&dl=pexels-mabel-amber-128299.jpg&fm=jpg');
     background-position: center; /* Center the image */
     background-repeat: no-repeat; /* Do not repeat the image */
     background-size: cover; /* Resize the background image to cover the entire container */
@@ -23,20 +18,20 @@ export const Title = styled.h1`
     justify-content: center;
 `;
 export const Wrapper  = styled.div`
-    width: 40%;
-    padding: 10px;
+    width: 25%;
+    padding: 50px;
     background-color: #fff;
 `;
 export const Form = styled.form`
     display: flex;
-    flex-wrap: wrap;
+    flex-direction: column;
 `;
 export const Input = styled.input`
     flex: 1;
     min-width: 40%;
     border: 3px solid gray;
     border-radius: 10px;
-    margin:20px 10px 0px 0px;
+    margin:10px 0px;
     padding: 10px;
 `;
 export const Agreement = styled.span`
@@ -45,14 +40,18 @@ export const Agreement = styled.span`
     margin: 20px 0px;
 `;
 export const Button = styled.button`
-    width: 30%;
+    /* width: 30%; */
     border: none;
     border-radius: 10px;
-    margin-top: 10px;
+    margin-top: 15px;
     padding: 15px 20px;
     color: white;
     background-color: black;
     cursor: pointer;
+    /* position: relative; */
+    display: grid;
+    justify-content: center;
+    
 
     &:hover {
         background-color: black;
@@ -62,6 +61,12 @@ export const Button = styled.button`
     }
 `;
 
-export const Link = styled.link`
-
+export const Link = styled.a`
+    margin: 10px 5px;
+    font-size: 20px;
+    font-weight: 300;
+    text-decoration: underline;
+    display: grid;
+    justify-content: center;
+    cursor: pointer;
 `;
