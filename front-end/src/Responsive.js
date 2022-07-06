e8697a2675c04ee595271450cab6eea181d69fcd
delaquash <olaide1191@gmail.com>
2022-07-06 11:20:50 +0100

    Added mobile and ipad media screen

--- front-end/src/Responsive.js
+++ front-end/src/Responsive.js
@@ -2,7 +2,22 @@ import { css } from "styled-components";
 
 export const mobile =(props) => {
     return css `
-        @media only screen and (min-width: 380px ){
+        @media only screen and (min-width: 320px ){
+            ${props}
+        }
+    `
+}
+
+export const tabletIpad = (props)=> {
+    return css `
+    @media only screen and (min-width: 768px) {
+        ${props}
+    }`
+}
+
+export const web = (props)=> {
+    return css`
+        @media only screen and (min-width: 1024px) {
             ${props}
         }
     `
